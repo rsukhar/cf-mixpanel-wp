f2519ea3723161c45205e9ca7bc8c876691b4a8f
rsukhar <ruslan@sukhar.info>
2017-12-19 08:37:58 +0300

    Added roistat counter

--- mixpaneljs.php
+++ mixpaneljs.php
@@ -78,3 +78,13 @@
         }
     });</script>
 <!-- end Mixpanel -->
+<!-- Roistat start -->
+<script>
+	(function(w, d, s, h, id) {
+		w.roistatProjectId = id; w.roistatHost = h;
+		var p = d.location.protocol == "https:" ? "https://" : "http://";
+		var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init";
+		var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+	})(window, document, 'script', 'cloud.roistat.com', '1faeeca23b4f09527e629f9202526a35');
+</script>
+<!-- Roistat End -->
